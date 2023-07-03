4173fb8a01ea3236f9a183850d4f028f9a75ae17
DarkRaider95 <tommy061295@gmail.com>
2023-07-03 23:10:12 +0200

    renamed some variables

--- PyazzaMarket/pyazzamarket/stock.py
+++ PyazzaMarket/pyazzamarket/stock.py
@@ -17,10 +17,10 @@ class Stock:
         self.logo = logo
         self.position = position
         self.font_stock_value = pygame.font.Font(None, 100)
-        self.font_prices = pygame.font.Font(None, 32)
+        self.fees = pygame.font.Font(None, 32)
 
     def draw(self, screen):
-        #Draw stock and spaces for logo and prices
+        #Draw stock and spaces for logo and fees
         stock_x = WIDTH // 2 - STOCK_WIDTH // 2
         stock_y = HEIGHT // 2 - STOCK_HEIGHT // 2
         logo_x = WIDTH // 2 - LOGO_WIDTH // 2
@@ -29,20 +29,20 @@ class Stock:
         price_y = logo_y + LOGO_HEIGHT + 150
         rect = pygame.Rect(stock_x, stock_y, STOCK_WIDTH, STOCK_HEIGHT)
         logorect = pygame.Rect(logo_x, logo_y, LOGO_WIDTH, LOGO_HEIGHT)
-        pricerect = pygame.Rect(price_x, price_y, PRICE_WIDTH, PRICES_HEIGHT)
+        feesrect = pygame.Rect(price_x, price_y, PRICE_WIDTH, PRICES_HEIGHT)
         pygame.draw.rect(screen, self.color, rect)
         pygame.draw.rect(screen, WHITE, logorect)
-        pygame.draw.rect(screen, WHITE, pricerect)
+        pygame.draw.rect(screen, WHITE, feesrect)
 
-        #Draw stock price and prices
+        #Draw stock price and fees
         stock_price = self.font_stock_value.render(str(self.stock_value) + "  SCUDI", True, WHITE)
         screen.blit(stock_price, (WIDTH // 2 - stock_price.get_width() // 2, logo_y + LOGO_HEIGHT + 50))
 
-        for i, value in enumerate(self.values):
+        for i in range(0,len(self.values)):
             #write string value
-            string_value = self.font_prices.render(Stock.string_values[i], True, BLACK)
+            string_value = self.fees.render(Stock.string_values[i], True, BLACK)
             screen.blit(string_value, (price_x + 10, price_y + 30 * i + 1))
             #write fee value for that string
-            value_price = self.font_prices.render(str(self.values[i]) + " SCUDI", True, BLACK)
-            screen.blit(value_price, (price_x + PRICE_WIDTH - value_price.get_width() - 10, price_y + 30 * i + 1))
+            fee_price = self.fees.render(str(self.values[i]) + " SCUDI", True, BLACK)
+            screen.blit(fee_price, (price_x + PRICE_WIDTH - fee_price.get_width() - 10, price_y + 30 * i + 1))
 
