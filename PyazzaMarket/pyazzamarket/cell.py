2bf0a569b6a363c50132d81c44910f48d6aa6f80
DarkRaider95 <tommy061295@gmail.com>
2023-07-04 22:26:09 +0200

    added surface to cells

--- PyazzaMarket/pyazzamarket/cell.py
+++ PyazzaMarket/pyazzamarket/cell.py
@@ -11,6 +11,7 @@ class Cell:
         self.cell_x = cell_x
         self.cell_y = cell_y
         self.cellImage = cellImage
+        self.surface = pygame.Surface((CELL_WIDTH, CELL_HEIGHT))
 
     def draw(self, screen):
         #if it's not a corner draw a cell
@@ -21,22 +22,23 @@ class Cell:
 
     def drawCell(self, screen):
         #Draw stock and spaces for logo and color
-        logo_x = self.cell_x + 2
-        logo_y = self.cell_y + 5
-        cell_color_x = self.cell_x
-        cell_color_y = self.cell_y + CELL_HEIGHT - CELL_COLOR_HEIGHT
-        cell_rect = pygame.Rect(self.cell_x, self.cell_y, CELL_WIDTH, CELL_HEIGHT)
+        logo_x = 2#self.cell_x + 2
+        logo_y = 5#self.cell_y + 5
+        cell_color_x = 0#self.cell_x
+        cell_color_y = CELL_HEIGHT - CELL_COLOR_HEIGHT#self.cell_y + CELL_HEIGHT - CELL_COLOR_HEIGHT
+        cell_rect = pygame.Rect(0, 0, CELL_WIDTH, CELL_HEIGHT)#pygame.Rect(self.cell_x, self.cell_y, CELL_WIDTH, CELL_HEIGHT)
         logorect = pygame.Rect(logo_x, logo_y, CELL_LOGO_WIDTH, CELL_LOGO_HEIGHT)
         colorrect = pygame.Rect(cell_color_x, cell_color_y, CELL_COLOR_WIDTH, CELL_COLOR_HEIGHT)
-        pygame.draw.rect(screen, WHITE, cell_rect)
-        pygame.draw.rect(screen, BLACK, logorect)
-        pygame.draw.rect(screen, self.color, colorrect)
+        pygame.draw.rect(self.surface, WHITE, cell_rect)
+        pygame.draw.rect(self.surface, BLACK, logorect)
+        pygame.draw.rect(self.surface, self.color, colorrect)
 
         #Draw stock price
         stock_price = self.font_stock_value.render("  "+ str(self.stock_value) + "\nSCUDI", True, BLACK)
-        price_x = (self.cell_x + CELL_WIDTH - (CELL_WIDTH // 2)) - (stock_price.get_width() // 2)
+        price_x = (CELL_WIDTH - (CELL_WIDTH // 2)) - (stock_price.get_width() // 2)
         price_y = cell_color_y - stock_price.get_height() - 10
-        screen.blit(stock_price, (price_x, price_y))
+        self.surface.blit(stock_price, (price_x, price_y))
+        screen.blit(self.surface, (self.cell_x, self.cell_y))
 
     def drawCellImage(self, screen):
         # Disponi l'immagine sulla finestra
